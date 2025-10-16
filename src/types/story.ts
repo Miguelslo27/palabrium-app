e6919a1c0648a3ffe5383bb51834836ba0372d20
Miguel Sosa <miguelmail2006@gmail.com>
2025-10-15 21:24:40 -0300

    Fix chapcter schema

--- src/types/story.ts
+++ src/types/story.ts
@@ -1,8 +1,15 @@
 export interface Chapter {
+  _id?: string;
+  storyId?: string;
   title: string;
   content: string;
   order?: number;
   published?: boolean;
+  publishedAt?: string | Date | null;
+  unPublishedAt?: string | Date | null;
+  publishedBy?: string | null;
+  unPublishedBy?: string | null;
+  createdAt?: string | Date | null;
 }
 
 export interface Story {
